9fddbc9929b92fce4911567e53dcbd70f33a3152
Kirill Fedotov <krftdt@gmail.com>
2017-09-02 18:32:26 +0300

    added first telegram bot

---
+++ lesson1/bot.py
@@ -0,0 +1,29 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
+
+def main():
+    updater = Updater("432283548:AAFcJ8EIaghmH2SyvDBy5Ngn892-6INbTZ8")
+
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
+    updater.start_polling()
+    updater.idle()
+
+def greet_user(bot, update):
+    text = 'Приветствую тебя, кожаный мешок!'
+    #print(update)
+    update.message.reply_text(text)
+
+def talk_to_me(bot, update):
+    user_text = update.message.text 
+    print(user_text)
+    update.message.reply_text(user_text)
+    
+main()
